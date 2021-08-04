46eaf54833407d1c61890c82f2f7e48fc3e2b650
Paul Angerer <48882462+dabauxi@users.noreply.github.com>
2021-08-04 13:59:50 +0200

    Added pip-tools for dependency management (#4)
    * Added requirements.in and requirements-dev.in, Added update-requirements.sh script, Modified setup.py to read requirements.in
    
    * Created requirements*.txt with pip-compile, Minor fix update-requirements.sh
    
    Co-authored-by: Paul Angerer <dabauxi@users.noreply.github.com>

--- core/setup.py
+++ core/setup.py
@@ -1,19 +1,17 @@
-"""Remove.bg Core Lib
-"""
-from pathlib import Path
+"""Remove.bg Core Lib"""
+
 from setuptools import setup
 from pkg_resources import parse_requirements
 
-install_requires = []
-with Path('requirements.txt').open() as requirements_txt:
-  for line in requirements_txt:
-    if not line.startswith('--'):
-      install_requires.append(line)
+with open("requirements.in") as requirements_in:
+    requirements = [line for line in requirements_in if not line.startswith("--")]
+
+install_requires = [str(requirement) for requirement in parse_requirements(requirements)]
 
 # See setup.cfg
 setup(
-  setup_requires=['pbr>=2.5', 'setuptools>=17.1'],
-  pbr=True,
-  install_requires=install_requires,
-  python_requires='>=3.6, <4'
+    setup_requires=['pbr>=2.5', 'setuptools>=17.1'],
+    pbr=True,
+    install_requires=install_requires,
+    python_requires='>=3.6'
 )
