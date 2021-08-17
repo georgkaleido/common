a655c814df1bbcf44ae8a421dad93a5612e98d01
Paul Angerer <48882462+dabauxi@users.noreply.github.com>
2021-08-17 14:01:07 +0200

    Removed @Q parameter operator (#5)
    Co-authored-by: Paul Angerer <dabauxi@users.noreply.github.com>

--- core/update-requirements.sh
+++ core/update-requirements.sh
@@ -9,11 +9,11 @@ pip install -U "pip-tools<7.0.0"
 
 echo "Updating requirements.txt. This may take a while..."
 pip-compile -U requirements.in --extra-index-url ${extra_index_url} --no-emit-index-url --no-header
-echo "--extra-index-url https://\${FURY_AUTH_TOKEN@Q}:@deps.kaleido.ai/pypi/" >> requirements.txt
+echo "--extra-index-url https://\${FURY_AUTH_TOKEN}:@deps.kaleido.ai/pypi/" >> requirements.txt
 echo "Succesfully updated requirements.txt"
 
 echo "Updating requirements-dev.txt. This may take a while..."
 pip-compile -U requirements.in requirements-dev.in -o requirements-dev.txt --extra-index-url ${extra_index_url} --no-emit-index-url --no-header
 echo $'pip\nwheel\nsetuptools' >> requirements-dev.txt
-echo "--extra-index-url https://\${FURY_AUTH_TOKEN@Q}:@deps.kaleido.ai/pypi/" >> requirements-dev.txt
+echo "--extra-index-url https://\${FURY_AUTH_TOKEN}:@deps.kaleido.ai/pypi/" >> requirements-dev.txt
 echo "Succesfully updated requirements-dev.txt"
