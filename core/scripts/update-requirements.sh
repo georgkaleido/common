fc56d936b8bdbd034f864bc8c3cf4387e6e2e1bb
Paul Angerer <48882462+dabauxi@users.noreply.github.com>
2021-11-15 17:40:58 +0100

    Torch dependencies with cuda (#58)
    * Set torch cuda versions
    
    * Yaml indentation
    
    * Yaml indentation
    
    Co-authored-by: Paul Angerer <dabauxi@users.noreply.github.com>

--- core/scripts/update-requirements.sh
+++ core/scripts/update-requirements.sh
@@ -15,30 +15,19 @@ echo "Checking for new version of pip-tools."
 pip install -U "pip-tools<7.0.0"
 
 echo "Updating requirements-deploy.txt. This may take a while..."
-pip-compile -U requirements-deploy.in -o requirements-deploy.txt --extra-index-url ${extra_index_url} --no-emit-index-url --no-header
+pip-compile -U requirements-deploy.in -o requirements-deploy.txt --find-links https://download.pytorch.org/whl/torch_stable.html --extra-index-url ${extra_index_url} --no-emit-index-url --no-header --find-links https://download.pytorch.org/whl/torch_stable.html
 # add --extra-index-url to beginning of file
 sed -i '1s/^/--extra-index-url https:\/\/\${FURY_AUTH_TOKEN}:@deps.kaleido.ai\/pypi\/\n\n/' requirements-deploy.txt
 echo "Successfully updated requirements-deploy.txt"
 
-echo "Removing torch,torchvision and torchtext dependencies from requirements-deploy.txt as already shipped in base image."
-# remove torch, torchvision dependencies from deployment dependencies as we have them in base image
-sed -i '/torch==/d' requirements-deploy.txt
-sed -i '/torchvision==/d' requirements-deploy.txt
-sed -i '/torchtext==/d' requirements-deploy.txt
-
 echo "Updating requirements-dev.txt. This may take a while..."
-pip-compile -U requirements-deploy.in requirements-dev.in -o requirements-dev.txt --extra-index-url ${extra_index_url} --no-emit-index-url --no-header
+pip-compile -U requirements-deploy.in requirements-dev.in -o requirements-dev.txt --find-links https://download.pytorch.org/whl/torch_stable.html --extra-index-url ${extra_index_url} --no-emit-index-url --no-header
 # add --extra-index-url to beginning of file
 sed -i '1s/^/--extra-index-url https:\/\/\${FURY_AUTH_TOKEN}:@deps.kaleido.ai\/pypi\/\n\n/' requirements-dev.txt
 echo "Successfully updated requirements-dev.txt"
 
-echo "Removing torch,torchvision and torchtext dependencies from requirements-dev.txt, install manually using conda."
-sed -i '/torch==/d' requirements-dev.txt
-sed -i '/torchvision==/d' requirements-dev.txt
-sed -i '/torchtext==/d' requirements-dev.txt
-
 echo "Updating requirements-train.txt. This may take a while..."
-pip-compile -U requirements-train.in -o requirements-train.txt --extra-index-url ${extra_index_url} --no-emit-index-url --no-header
+pip-compile -U requirements.in requirements-train.in -o requirements-train.txt --extra-index-url ${extra_index_url} --no-emit-index-url --no-header
 # add --extra-index-url to beginning of file
 sed -i '1s/^/--extra-index-url https:\/\/\${FURY_AUTH_TOKEN}:@deps.kaleido.ai\/pypi\/\n\n/' requirements-train.txt
 echo "Successfully updated requirements-train.txt"
@@ -48,7 +37,6 @@ echo "Removing torch,torchvision and torchtext dependencies from requirements-tr
 sed -i '/torch==/d' requirements-train.txt
 sed -i '/torchvision==/d' requirements-train.txt
 sed -i '/torchtext==/d' requirements-train.txt
-
 
 echo "Updating requirements-eval.txt. This may take a while..."
 pip-compile -U requirements.in requirements-eval.in -o requirements-eval.txt --extra-index-url ${extra_index_url} --no-emit-index-url --no-header
